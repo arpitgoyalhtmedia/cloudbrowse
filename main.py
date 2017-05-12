f2e0f3f8afe93b1f8d0a920151a1e6a4456807cd
Arpit Goyal <arpitgoyal@Arpits-MacBook-Air.local>
2017-05-12 02:23:47 -0700

    included the project files

---
+++ main.py
@@ -0,0 +1,13 @@
+# inbuild python imports
+import argparse
+
+# third party imports
+
+
+if __name__ == '__main__':
+    """
+    The main function
+    """
+
+    parser = argparse.ArgumentParser(description = "argument parser for the cloudbrowse project")
+    parser.add_argument()
